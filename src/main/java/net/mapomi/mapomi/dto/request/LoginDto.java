330f5cecc72676b94eb8d5c4d4f30cdf57ea9196
inseo <sis000512@gmail.com>
2023-05-22 23:28:08 +0900

    fix: join process(2)

--- src/main/java/net/mapomi/mapomi/dto/request/LoginDto.java
+++ src/main/java/net/mapomi/mapomi/dto/request/LoginDto.java
@@ -1,12 +1,12 @@
 package net.mapomi.mapomi.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginDto {
     private String nickName;
-
-    public LoginDto(String nickName) {
-        this.nickName = nickName;
-    }
 }
