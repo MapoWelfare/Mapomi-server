4bbd8920fec024729e00fdc2b02dea65d33bf788
inseo <sis000512@gmail.com>
2023-05-24 23:11:47 +0900

    feat: help post

--- src/test/java/net/mapomi/mapomi/TestSuite.java
+++ src/test/java/net/mapomi/mapomi/TestSuite.java
@@ -1,6 +1,6 @@
 package net.mapomi.mapomi;
 
-import net.mapomi.mapomi.service.PostServiceTest;
+import net.mapomi.mapomi.service.AccompanyServiceTest;
 import net.mapomi.mapomi.service.UserServiceTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         UserServiceTest.class,
-        PostServiceTest.class
+        AccompanyServiceTest.class
 })
 @SpringBootTest
 public class TestSuite {
